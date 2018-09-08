eb42d01511ee4398db67dcd5ed44c4aa28a0089b
Min <mbeh@andrew.cmu.edu>
2018-09-07 23:48:39 -0400

    Reduce height of plotly graph for better readability

--- hw1-mbeh.R
+++ hw1-mbeh.R
@@ -87,7 +87,7 @@ server <- function(input, output) {
       theme_gray()
     
     ggplotly(plot, tooltip = "text") %>%
-      layout(height = 800, legend = list(orientation = "h", x = 0, y = -0.1))
+      layout(height = 600, legend = list(orientation = "h", x = 0, y = -0.1))
   })
   
   output$table <- DT::renderDataTable({
